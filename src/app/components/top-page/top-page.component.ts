f3cf4f48e38517dabddb5028eccddc014039285b
masaya.ishioka <masaya.ishioka@pa-rk.co.jp>
2020-02-15 18:04:21 +0900

    Add view css

--- src/app/components/top-page/top-page.component.ts
+++ src/app/components/top-page/top-page.component.ts
@@ -8,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class TopPageComponent implements OnInit {
 
   items = [
-    {name: "test1", price: 600, link: "aaaa"},
-    {name: "test1", price: 600, link: "aaaa"},
-    {name: "test1", price: 600, link: "aaaa"},
-    {name: "test1", price: 600, link: "aaaa"},
-    {name: "test1", price: 600, link: "aaaa"},
+    {
+      title: "スッキリわかるJava入門第3版",
+      price: "2860円",
+      link: "https://hb.afl.rakuten.co.jp/hgc/g00q0727.80x1s980.g00q0727.80x1tb87/?pc=https%3A%2F%2Fbooks.rakuten.co.jp%2Frb%2F16099007%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Frms%2Fmsv%2FItem%3Fn%3D16099007%26surl%3Dbook",
+      discription: "圧倒的人気Ｎｏ．１入門書の増補改訂版！コレクションを追加！基本文法やオブジェクト指向の「なぜ？」が必ずわかる！",
+      saleDate: "2019/11/01",
+      publisherName: "インプレス",
+      author: "中山清喬/国本大悟"},
+    {title: "スッキリわかるJava入門第3版", price: "2860円", link: "https://hb.afl.rakuten.co.jp/hgc/g00q0727.80x1s980.g00q0727.80x1tb87/?pc=https%3A%2F%2Fbooks.rakuten.co.jp%2Frb%2F16099007%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Frms%2Fmsv%2FItem%3Fn%3D16099007%26surl%3Dbook", discription: "圧倒的人気Ｎｏ．１入門書の増補改訂版！コレクションを追加！基本文法やオブジェクト指向の「なぜ？」が必ずわかる！", saleDate: "2019/11/01", publisherName: "インプレス", author: "中山清喬/国本大悟"},
+    {title: "スッキリわかるJava入門第3版", price: "2860円", link: "https://hb.afl.rakuten.co.jp/hgc/g00q0727.80x1s980.g00q0727.80x1tb87/?pc=https%3A%2F%2Fbooks.rakuten.co.jp%2Frb%2F16099007%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Frms%2Fmsv%2FItem%3Fn%3D16099007%26surl%3Dbook", discription: "圧倒的人気Ｎｏ．１入門書の増補改訂版！コレクションを追加！基本文法やオブジェクト指向の「なぜ？」が必ずわかる！", saleDate: "2019/11/01", publisherName: "インプレス", author: "中山清喬/国本大悟"},
+    {title: "スッキリわかるJava入門第3版", price: "2860円", link: "https://hb.afl.rakuten.co.jp/hgc/g00q0727.80x1s980.g00q0727.80x1tb87/?pc=https%3A%2F%2Fbooks.rakuten.co.jp%2Frb%2F16099007%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Frms%2Fmsv%2FItem%3Fn%3D16099007%26surl%3Dbook", discription: "圧倒的人気Ｎｏ．１入門書の増補改訂版！コレクションを追加！基本文法やオブジェクト指向の「なぜ？」が必ずわかる！", saleDate: "2019/11/01", publisherName: "インプレス", author: "中山清喬/国本大悟"},
+    {title: "スッキリわかるJava入門第3版", price: "2860円", link: "https://hb.afl.rakuten.co.jp/hgc/g00q0727.80x1s980.g00q0727.80x1tb87/?pc=https%3A%2F%2Fbooks.rakuten.co.jp%2Frb%2F16099007%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Frms%2Fmsv%2FItem%3Fn%3D16099007%26surl%3Dbook", discription: "圧倒的人気Ｎｏ．１入門書の増補改訂版！コレクションを追加！基本文法やオブジェクト指向の「なぜ？」が必ずわかる！", saleDate: "2019/11/01", publisherName: "インプレス", author: "中山清喬/国本大悟"},
+    {title: "スッキリわかるJava入門第3版", price: "2860円", link: "https://hb.afl.rakuten.co.jp/hgc/g00q0727.80x1s980.g00q0727.80x1tb87/?pc=https%3A%2F%2Fbooks.rakuten.co.jp%2Frb%2F16099007%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Frms%2Fmsv%2FItem%3Fn%3D16099007%26surl%3Dbook", discription: "圧倒的人気Ｎｏ．１入門書の増補改訂版！コレクションを追加！基本文法やオブジェクト指向の「なぜ？」が必ずわかる！", saleDate: "2019/11/01", publisherName: "インプレス", author: "中山清喬/国本大悟"},
+    {title: "スッキリわかるJava入門第3版", price: "2860円", link: "https://hb.afl.rakuten.co.jp/hgc/g00q0727.80x1s980.g00q0727.80x1tb87/?pc=https%3A%2F%2Fbooks.rakuten.co.jp%2Frb%2F16099007%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Frms%2Fmsv%2FItem%3Fn%3D16099007%26surl%3Dbook", discription: "圧倒的人気Ｎｏ．１入門書の増補改訂版！コレクションを追加！基本文法やオブジェクト指向の「なぜ？」が必ずわかる！", saleDate: "2019/11/01", publisherName: "インプレス", author: "中山清喬/国本大悟"},
   ]
 
   constructor() { }
